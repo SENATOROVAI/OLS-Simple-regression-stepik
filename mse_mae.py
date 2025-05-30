f08d7bfc5802069ab44f47445ca72d5c371ee49c
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2025-05-30 22:44:36 +0300

    Create mse_mae.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

---
+++ mse_mae.py
@@ -0,0 +1,11 @@
+import numpy as np
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import matplotlib.pyplot as plt
+
+y_true = np.array([10, 10.5, 11])
+
+y_pred = np.array([10.2, 10.4, 11.1])
+
+errors = y_true - y_pred
+abs_errors = np.abs(errors)
+squared_errors = errors ** 2
